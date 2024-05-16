bb892a0b7fcd9a1a32bc992316ccf588c8cb1fde
poojithakeerthipati <poojikeerthipati123@gmail.com>
2024-05-16 10:18:03 +0530

    created barreling for entity

---
+++ Entity/__init__.py
@@ -0,0 +1,4 @@
+from .employee import Employee
+from .financial_record import Financialrecord
+from .payroll import Payroll
+from .tax import Tax
--- Entity/tax.py
+++ Entity/tax.py
@@ -1,4 +1,4 @@
-class tax:
+class Tax:
     def __init__(self,employee_id,tax_year,taxable_income,tax_amount):
         self.employee_id = employee_id
         self.tax_year = tax_year
